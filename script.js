13bb18ac1a108d931c75c807a9c484648b8647cb
Shalom Hunukumbura <114820357+ShalomHunukumbura@users.noreply.github.com>
2023-04-03 17:58:53 +0530

    score points

--- script.js
+++ script.js
@@ -26,32 +26,51 @@ function game(){
 
        }           
 }
-
+let score=0;
 function singleRound(playerSelection,computerSelection){
     playerSelection = userInput();
-    
+   
     if(playerSelection == 'rock' && computerSelection == 'Paper'){
         console.log('You lose! Paper beats Rock');
+        score--;
+
     }else if(playerSelection== 'rock' && computerSelection == 'Scissor' ){
         console.log('you win! Rock beats scissor');
+        score++;
     }else if(playerSelection=='rock' && computerSelection =='Rock'){
         console.log('It is a Draw! Try Again');
     }if(playerSelection == 'paper' && computerSelection == 'Rock'){
         console.log('You win! Paper beats Rock');
+        score++;
     }else if(playerSelection== 'paper' && computerSelection == 'Scissor' ){
         console.log('you loose! scissor beats paper');
+        score--;
     }else if(playerSelection=='paper' && computerSelection =='Paper'){
         console.log('It is a Draw! Try Again');
     }if(playerSelection == 'scissor' && computerSelection == 'Paper'){
         console.log('You win! Scissor beats paper');
+        score++;
     }else if(playerSelection== 'scissor' && computerSelection == 'Rock' ){
         console.log('you loose! Rock beats scissor');
+        score--;
     }else if(playerSelection=='scissor' && computerSelection =='Scissor'){
         console.log('It is a Draw! Try Again');
     }
+   
 
 }
 
 
 const computerSelection=getComputerChoice();
 game();
+scorePoints();
+
+function scorePoints(){
+    console.log(`Score: ${score}`)
+    if(score<3){
+        console.log('You Lose! Computer Wins!!!');
+    }else{
+        console.log('You Win!, Congratulations!!!');
+    }
+}
+
