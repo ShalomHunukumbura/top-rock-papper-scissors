199eaa011cabc46b5997bdf2ce55b94c7eebeeb1
Shalom Hunukumbura <114820357+ShalomHunukumbura@users.noreply.github.com>
2023-09-01 18:31:20 +0530

    pre-final touches
    added score,computer score,resetting game function

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const btnChoices = document.querySelectorAll('.btn_choice');
-        const result = document.querySelector('.result');
-        
+const result = document.querySelector('.result');
+const playerScore = document.querySelector('.player-score');
+const computerScore = document.querySelector('.comp-score');
+let compScore = 0;
+let roundScore = 0;
         
         let randChoice;
         let round = 0;
@@ -26,39 +29,99 @@ const btnChoices = document.querySelectorAll('.btn_choice');
 
                     round++;
                 } else {
-                    result.textContent = "Game over!";
+                    alert(`Gamw over. ${winner()}`);
+                    document.getElementById('newGameButton').style.display = 'block';
+                    
                 }
             });
         });
 
+        document.getElementById('newGameButton').addEventListener('click' ,()=>{
+            round = 0;
+            roundScore = 0;
+            compScore = 0;
+
+            result.innerHTML = '';
+            playerScore.innerHTML = roundScore;
+            computerScore.innerHTML = compScore;
+
+            document.getElementById('newGameButton').style.display = 'none';
+        });
+
+        function updateScore(result){
+            if(result == 'win'){
+                roundScore++;
+            }else if(result == 'lose'){
+                roundScore--;
+            }
+            playerScore.innerHTML = roundScore;
+
+        }
+
+        function updateComputerScore(result){
+            if(result=='win'){
+                compScore++;
+            }else if(result=='lose'){
+                compScore--;
+            }
+            computerScore.innerHTML=compScore;
+        }
 
         function rockChoice(compChoice){
             if (compChoice == 1){
-                result.textContent = "It is a draw!!";
+                result.innerHTML = "It is a draw!!";
+                updateScore('draw');
+                updateComputerScore('draw');
             }else if(compChoice == 2){
-                result.textContent = "You loose, paper beats rock!!"
+                result.innerHTML = "You loose, paper beats rock!!"
+                updateScore("lose");
+                updateComputerScore('win');
             }else if(compChoice == 3){
-                result.textContent = "You win, Rock beats Scissors!!";
+                result.innerHTML = "You win, Rock beats Scissors!!";
+                updateScore('win');
+                updateComputerScore('lose');
             }
         }
 
         function paperChoice(compChoice){
             if (compChoice == 1){
-                result.textContent = "You win Paper beats Rock!!";
+                result.innerHTML = "You win Paper beats Rock!!";
+                updateScore('win');
+                updateComputerScore('lose');
             }else if(compChoice == 2){
-                result.textContent = "It is a draw!!"
+                result.innerHTML = "It is a draw!!"
+                updateScore('draw');
+                updateComputerScore('draw');
             }else if(compChoice == 3){
-                result.textContent = "You loose, Scissor beats Paper!!";
+                result.innerHTML = "You loose, Scissor beats Paper!!";
+                updateScore('lose');
+                updateComputerScore('win');
             }
         }
 
         function scissorChoice(compChoice){
             if (compChoice == 1){
-                result.textContent = "You loose, Rock beats Scissor!!";
+                result.innerHTML = "You loose, Rock beats Scissor!!";
+                updateScore('win');
+                updateComputerScore('lose')
             }else if(compChoice == 2){
-                result.textContent = "You win, Scissor beats Paper!!"
+                result.innerHTML = "You win, Scissor beats Paper!!"
+                updateScore('lose');
+                updateComputerScore('win');
             }else if(compChoice == 3){
-                result.textContent = "It is a draw!!";
+                result.innerHTML = "It is a draw!!";
+                updateScore('draw');
+                updateComputerScore('draw');
+            }
+        }
+
+        function winner(){
+            if(roundScore>compScore){
+                return "Player wins";
+            }else if(compScore>roundScore){
+                return 'computer Wins';
+            }else{
+                return 'It is a draw'
             }
         }
       
